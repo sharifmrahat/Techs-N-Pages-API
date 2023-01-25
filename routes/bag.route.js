528aff7b1fe9e2ad6d2ce04b0d0f443a3903aff1
sharifmrahat <sharifmrahat@gmail.com>
2023-01-25 21:50:44 +0600

    cant't create bag without signup is added

--- routes/bag.route.js
+++ routes/bag.route.js
@@ -4,23 +4,7 @@ const bagController = require("../controllers/bag.controller");
 const { verifyToken } = require("../middleware/verifyToken");
 // const authorization = require("../utils/authorization");
 
-router.route("/add").post(verifyToken, bagController.createBag);
+// router.route("/add").post(verifyToken, bagController.createBag);
 
-
-// router.route("/login").post(userController.login);
-
-// router.get("/currentUser", verifyToken, userController.getCurrentUser);
-
-// router.route("/update").patch(verifyToken, userController.updateUser);
-
-
-// router.get("/all", verifyToken, authorization("admin"), userController.getAllUser);
-// router
-//   .route("/update/role/:id")
-//   .patch(verifyToken, authorization("admin"), userController.updateUserRole);
-
-// router
-//   .route("/delete/:id")
-//   .delete(verifyToken, authorization("admin"), userController.deleteUser);
 
 module.exports = router;
